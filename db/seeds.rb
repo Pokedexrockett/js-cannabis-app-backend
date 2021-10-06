74de43ea4acccc7606841d83d3ab04300463a1f1
Dexter <dex429.dc@gmail.com>
2021-10-05 20:08:15 -0400

    add cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 holistic = Grower.create(name:"Holistic");
-Harvest = Grower.create(name:"Harvest");
-Verano = Grower.create(name:"Verano");
+harvest = Grower.create(name:"Harvest");
+verano = Grower.create(name:"Verano");
 
-gdp = Strain.create(name: "GDP", category: "Indica", thc: "20.1", cbd: "0.03", grower: holistic)
-biscotti_gelato = Strain.create(name: "Biscotti Gelato", category: "Hybrid", thc: "22.1", cbd: "0.03", grower: holistic)
-royal_wedding_cake = Strain.create(name: "Royal Wedding Cake", category: "Hybrid", thc: "19.1", cbd: "0.03", grower: holistic)
-mac_and_cheese = Strain.create(name: "Mac And Cheese", category: "Sativa", thc: "24.1", cbd: "0.03", grower: holistic)
-white_mac = Strain.create(name: "White Mac", category: "Hybrid", thc: "27.1", cbd: "0.03", grower: holistic)
+Strain.create(name: "GDP", category: "Indica", thc: "20.1", cbd: "0.03", grower: holistic);
+Strain.create(name: "Biscotti Gelato", category: "Hybrid", thc: "22.1", cbd: "0.03", grower: holistic);
+Strain.create(name: "Royal Wedding Cake", category: "Hybrid", thc: "19.1", cbd: "0.03", grower: holistic);
+Strain.create(name: "Mac And Cheese", category: "Sativa", thc: "24.1", cbd: "0.03", grower: holistic);
+Strain.create(name: "White Mac", category: "Hybrid", thc: "27.1", cbd: "0.03", grower: holistic);
+
+Strain.create(name: "Cinderella 99", category: "Sativa", thc: "22.1", cbd: "0.03", grower: harvest);
+Strain.create(name: "Lavender Jones", category: "Hybrid", thc: "21.1", cbd: "0.03", grower: harvest);
+Strain.create(name: "Gelato", category: "Hybrid", thc: "18.1", cbd: "0.03", grower: harvest);
+Strain.create(name: "Girl Scout Cookies", category: "Hybrid", thc: "23.1", cbd: "0.03", grower: harvest);
+Strain.create(name: "Nom Nom", category: "Hybrid", thc: "16.1", cbd: "0.03", grower: harvest);
+
+Strain.create(name: "Pineapple Kush", category: "Sativa", thc: "25.1", cbd: "0.03", grower: verano);
+Strain.create(name: "Purple Punch", category: "Indica", thc: "20.1", cbd: "0.03", grower: verano);
+Strain.create(name: "Bruce Banner", category: "Sativa", thc: "27.1", cbd: "0.03", grower: verano);
+Strain.create(name: "Corleone Kush", category: "Indica", thc: "20.1", cbd: "0.03", grower: verano);
+Strain.create(name: "Harley Tsu", category: "Hybrid", thc: "5.1", cbd: "20.03", grower: verano);
 
 
 
 
-grower = Grower.create(name: "Holistic")
-grower2 = Grower.create(name: "Harvest")
-grower3 = Grower.create(name: "Verano")
-
-strain = Strain.create(name: "GDP", category: "Sativa", thc: "20.1", cbd: "0.03", grower: grower)
