81689437f0c599c5e7d77c391d9ea486e18ec97e
Dexter <dex429.dc@gmail.com>
2021-10-06 21:14:18 -0400

    add destroy method

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -4,14 +4,34 @@ class StrainsController < ApplicationController
         render json: strains
     end
 
+    def show
+        strain = Strain.find_by_id(params[:id])
+        render json: strain
+    end
+
     def create
         strain = Strain.new(strain_params)
-        byebug
+        
         if strain.save
             render json: strain
         else
             render json: {error: "Couldn't be saved"}
         end
+    end
+
+    def update
+        strain = Strain.find_by_id(params[:id])
+        if strain.update(strain_params)
+            render json: strain
+        else
+            render json: {error: "Couldn't Update"}
+        end
+    end
+
+    def destroy
+        strain = Strain.find_by_id(params[:id])
+        strain.destroy
+        render json: {message: "Successfully Deleted #{strain.name}"}
     end
 
     private
